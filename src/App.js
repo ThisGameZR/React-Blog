e48f8034aaae4f0c6be059000f87fe9eddbb8d57
ThisGameZR <timezazare@gmail.com>
2021-11-01 04:10:00 +0700

    cta

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+<<<<<<< HEAD
 import logo from "./logo.svg";
 import "./App.css";
 import Blog from "./container/blog/Blog";
+=======
+import React from 'react';
+import Footer from './container/footer/Footer';
+import './App.css';
+import Blog from './container/blog/Blog';
+import CTA from './components/cta/CTA';
+>>>>>>> 6f9db3f (cta component)
 
 function App() {
   return (
     <div className="App">
+      <CTA />
       <Blog />
       <Footer />
     </div>
