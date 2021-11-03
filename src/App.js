42076674b9fa9faf6e39b244e25f742e8b9c2751
ThisGameZR <timezazare@gmail.com>
2021-11-04 03:35:00 +0700

    possibility section

--- src/App.js
+++ src/App.js
@@ -8,11 +8,16 @@ import Footer from './container/footer/Footer';
 import './App.css';
 import Blog from './container/blog/Blog';
 import CTA from './components/cta/CTA';
+<<<<<<< HEAD
 >>>>>>> 6f9db3f (cta component)
+=======
+import Possibility from './container/possibility/Possibility';
+>>>>>>> 9683040 (possibility section)
 
 function App() {
   return (
     <div className="App">
+      <Possibility />
       <CTA />
       <Blog />
       <Footer />
