b2b1c0c10e401a928659ed7f50a4a86055c47e54
ThisGameZR <timezazare@gmail.com>
2021-11-10 04:48:00 +0700

    feature section

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ import CTA from './components/cta/CTA';
 >>>>>>> 6f9db3f (cta component)
 =======
 import Possibility from './container/possibility/Possibility';
+<<<<<<< HEAD
 >>>>>>> 9683040 (possibility section)
+=======
+import Features from './container/features/Features';
+>>>>>>> 67431ef (features section)
 
 function App() {
   return (
     <div className="App">
+      <Features />
       <Possibility />
       <CTA />
       <Blog />
