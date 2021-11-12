b9529f1f8f7b50061750f13a97c50b47725baf57
ThisGameZR <timezazare@gmail.com>
2021-11-13 06:18:00 +0700

    what is section

--- src/App.js
+++ src/App.js
@@ -16,11 +16,16 @@ import Possibility from './container/possibility/Possibility';
 >>>>>>> 9683040 (possibility section)
 =======
 import Features from './container/features/Features';
+<<<<<<< HEAD
 >>>>>>> 67431ef (features section)
+=======
+import WhatGPT3 from './container/whatGPT3/WhatGPT3';
+>>>>>>> 9624a1b (what is section)
 
 function App() {
   return (
     <div className="App">
+      <WhatGPT3 />
       <Features />
       <Possibility />
       <CTA />
