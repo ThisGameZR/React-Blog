faa10a1a554646c7f43bd7aa553bbeca6958134e
ThisGameZR <timezazare@gmail.com>
2023-04-22 18:06:37 +0700

    solve defects

--- src/App.js
+++ src/App.js
@@ -1,31 +1,7 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import React from "react";
-import Footer from "./container/footer/Footer";
-import Blog from "./container/blog/Blog";
-import CTA from "./components/cta/CTA";
-import Possibility from "./container/possibility/Possibility";
-import Features from "./container/features/Features";
-import WhatGPT3 from "./container/whatGPT3/WhatGPT3";
-import Brand from "./components/brand/Brand";
-import Header from "./container/header/Header";
-import Navbar from "./components/navbar/Navbar";
-import "./App.css";
-=======
-import React from 'react';
-=======
-import React from "react";
->>>>>>> 1d79f35 (resolve merge conflict)
-
 import { Footer, Blog, Possibility, Features, WhatIs, Header } from "./containers";
 import { CTA, Brand, Navbar } from "./components";
-
-<<<<<<< HEAD
-import './App.css';
->>>>>>> 848d075 (adjust contents)
-=======
 import "./App.css";
->>>>>>> 1d79f35 (resolve merge conflict)
 
 const App = () => (
   <div className="App">
